618ef1f68bd9f503ed7eebeeb0a0fe04a8444e51
Prince Mewal <mewalpriyanshu0@gmail.com>
2026-01-29 18:31:43 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project that would predict the delivery time based upon distance, traffic, weather and all.',
+    author='Priyanshu Mewal',
+    license='',
+)
